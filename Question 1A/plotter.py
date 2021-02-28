379549e12d98067aca7f3c67fc73b197da1b56f6
Abhiroop25902 <abhiroop.m25902@gmail.com>
2021-02-28 21:01:09 +0530

    Added Comment in .py files, better understanding

--- Question 1A/plotter.py
+++ Question 1A/plotter.py
@@ -1,13 +1,15 @@
-import numpy as np
-import csv
-import matplotlib.pyplot as plt
+import numpy as np  # for arrays
+import csv  # for reading .csv files
+import matplotlib.pyplot as plt  # for plotting
 
-random_numbers = np.zeros(201)
+# .csv has 200 values, so defining 200 value array
+random_numbers = np.zeros(200)
 
-with open('./Question 1A/uniform_distribution.csv') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    line_count = 0
-    for row in csv_reader:
+with open('./Question 1A/uniform_distribution.csv') as csv_file:  # while .csv file is open
+    csv_reader = csv.reader(csv_file, delimiter=',')  # define .csv file reader
+    line_count = 0  # linecount made as index of array
+    for row in csv_reader:  # parse through every lines
+        # first value of every line is the random number
         random_numbers[line_count] = row[0]
         line_count = line_count + 1
     print(f"Processed {line_count} lines.")
