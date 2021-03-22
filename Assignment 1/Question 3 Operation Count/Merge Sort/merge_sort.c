b924e6631ad5a0969d4ed3e1c776c393ef97dcff
Abhiroop25902 <abhiroop.m25902@gmail.com>
2021-03-22 22:16:38 +0530

    added count printing and data collect from dataset

--- Assignment 1/Question 3 Operation Count/Merge Sort/merge_sort.c
+++ Assignment 1/Question 3 Operation Count/Merge Sort/merge_sort.c
@@ -3,17 +3,26 @@
 #include <time.h>
 #include <math.h>
 
-void merge(int *arr, int initial, int mid, int final)
+const int DATASET_SIZE = 10e6;
+
+void merge(int *arr, int initial, int mid, int final, long *count)
 {
     int size1 = mid - initial + 1;
     int arr1[size1];
     for (int i = 0; i < size1; i++)
+    {
         arr1[i] = arr[initial + i];
+        (*count)++;
+    }
 
     int size2 = final - mid;
     int arr2[size2];
+
     for (int i = 0; i < size2; i++)
+    {
         arr2[i] = arr[mid + 1 + i];
+        (*count)++;
+    }
 
     int arr1_pos = 0;
     int arr2_pos = 0;
@@ -26,13 +35,16 @@ void merge(int *arr, int initial, int mid, int final)
             arr[arr_pos] = arr1[arr1_pos];
             arr1_pos++;
             arr_pos++;
+            (*count)++;
         }
         else
         {
             arr[arr_pos] = arr2[arr2_pos];
             arr2_pos++;
             arr_pos++;
+            (*count)++;
         }
+        (*count) += 2;
     }
 
     while (arr1_pos < size1)
@@ -40,6 +52,7 @@ void merge(int *arr, int initial, int mid, int final)
         arr[arr_pos] = arr1[arr1_pos];
         arr1_pos++;
         arr_pos++;
+        (*count)++;
     }
 
     while (arr2_pos < size2)
@@ -47,18 +60,19 @@ void merge(int *arr, int initial, int mid, int final)
         arr[arr_pos] = arr2[arr2_pos];
         arr2_pos++;
         arr_pos++;
+        (*count)++;
     }
 }
 
 //sorts array of int type in ascending order, put initial as 0, and final as size -1
-void merge_sort(int *arr, int initial, int final)
+void merge_sort(int *arr, int initial, int final, long *count)
 {
     if (initial < final)
     {
         int mid = (initial + final) / 2;
-        merge_sort(arr, initial, mid);
-        merge_sort(arr, mid + 1, final);
-        merge(arr, initial, mid, final);
+        merge_sort(arr, initial, mid, count);
+        merge_sort(arr, mid + 1, final, count);
+        merge(arr, initial, mid, final, count);
     }
 }
 
@@ -72,22 +86,50 @@ int is_array_sorted(int *arr, int length)
     return 1;
 }
 
+void text_to_arr(int *arr, int n)
+{
+
+    FILE *fin = fopen("./normal_distribution.csv", "r");
+
+    int temp = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int skip_line = rand() % 10;
+
+        for (int j = 0; j < skip_line; j++)
+            fscanf(fin, "%d\n", &temp);
+
+        fscanf(fin, "%d\n", &temp);
+        arr[i] = temp;
+    }
+}
+
 void main()
 {
     srand(time(0));
     for (int i = 0; i < 100; i++)
     {
-        int n = floor(pow(2, 5));
+        int n = floor(pow(2, 3));
         int a[n];
-        for (int i = 0; i < n; i++)
-            a[i] = rand() % 100;
 
-        float start_time = clock();
-        merge_sort(a, 0, n - 1);
-        float end_time = clock();
+        text_to_arr(a, n);
+
+        // for (int i = 0; i < n; i++)
+        //     printf("%d ", a[i]);
+        // printf("\n");
+
+        long count = 0;
+        // float start_time = clock();
+        merge_sort(a, 0, n - 1, &count);
+        // float end_time = clock();
+
+        // for (int i = 0; i < n; i++)
+        //     printf("%d ", a[i]);
+        // printf("\n\n");
 
         if (is_array_sorted(a, sizeof(a) / sizeof(int)) == 1)
-            printf("Run %d: %0.4fms\n", i + 1, (end_time - start_time) * 1000 / CLOCKS_PER_SEC);
+            // printf("Run %d: %0.4fms\n", i + 1, (end_time - start_time) * 1000 / CLOCKS_PER_SEC);
+            printf("Run %d: %ld comparisions\n", i + 1, count);
         else
         {
             printf("Array Not Sorted\n");
