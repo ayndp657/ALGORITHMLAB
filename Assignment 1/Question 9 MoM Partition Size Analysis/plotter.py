4e713358ab78b0a606426865b240ddb60fb515cd
Abhiroop25902 <abhiroop.m25902@gmail.com>
2021-04-04 14:51:57 +0530

    completed question 9

--- Assignment 1/Question 9 MoM Partition Size Analysis/plotter.py
+++ Assignment 1/Question 9 MoM Partition Size Analysis/plotter.py
@@ -1,25 +1,21 @@
-from datetime import time
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib
-from pandas.core.indexing import is_label_like
 
 uniform_df = pd.read_csv('MoM_uniform_obs.csv')
 normal_df = pd.read_csv('MoM_normal_obs.csv')
 
 n_uniform = uniform_df['arr_size'].to_numpy()
-time_uniform = uniform_df['avg_time'].to_numpy()
+part_uniform = uniform_df['avg_partition_size'].to_numpy()
 
 n_normal = normal_df['arr_size'].to_numpy()
-time_normal = normal_df['avg_time'].to_numpy()
+part_normal = normal_df['avg_partition_size'].to_numpy()
 
 plt.figure()
-plt.plot(n_uniform, time_uniform, label='Uniform')
-plt.plot(n_normal, time_normal,  label='Normal')
+plt.plot(n_uniform, part_uniform/n_uniform, '.', label='Uniform')
+plt.plot(n_normal, part_normal/n_normal, '.', label='Normal')
 plt.legend()
 plt.xlabel('arr_size (n)')
-plt.ylabel('Time in ms')
+plt.ylabel(r'$\frac{partitions}{n}$')
 plt.title('Time vs Array Size in MoM')
 
 plt.savefig('observation.png')
