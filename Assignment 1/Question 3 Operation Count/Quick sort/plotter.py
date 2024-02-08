49a81967c1504e831fb6b5b3d9f1c9324868995b
ayndp657 <140968393+ayndp657@users.noreply.github.com>
2024-02-08 17:58:41 +0530

    Update plotter.py

--- Assignment 1/Question 3 Operation Count/Quick sort/plotter.py
+++ Assignment 1/Question 3 Operation Count/Quick sort/plotter.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib
 from pandas.core.indexing import is_label_like
 
-uniform_df = pd.read_csv('quick_uniform_obs.csv')
-normal_df = pd.read_csv('quick_normal_obs.csv')
+uniform_df = pd.read_csv('quickuniform_obs.csv')
+normal_df = pd.read_csv('quicknormal_obs.csv')
 
 n_uniform = uniform_df['num_elements'].to_numpy()
 comp_uniform = uniform_df['avg_comparison'].to_numpy()
@@ -19,7 +19,7 @@ comp_ratio_normal = comp_normal/(n_normal*np.log2(n_normal))
 
 fig, axis = plt.subplots(2, 1, figsize=(8, 5))
 
-fig.suptitle('Comparision and Time Ratio')
+fig.suptitle('Comparison and Time Ratio')
 
 axis[0].plot(comp_ratio_uniform, 'o-', label='Uniform')
 axis[0].plot(comp_ratio_normal, 'o--', label='Normal')
@@ -36,4 +36,4 @@ axis[1].set_ylabel(r'$\frac{time}{n*lgn}$', rotation=0, labelpad=13)
 
 plt.xlabel(r'n [array size = $2^n$]')
 
-plt.savefig('observation.png')
+plt.savefig('observationquick.png')
